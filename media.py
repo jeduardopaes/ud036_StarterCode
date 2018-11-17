9645226a039de994b2a7ea4549ba7b13a1260f0b
Jose Eduardo Paes <j.eduardopaes@gmail.com>
2018-11-17 18:45:47 -0200

    Code respects all PEP 8 but is ugly.

--- media.py
+++ media.py
@@ -1,19 +1,20 @@
 import webbrowser
 
+
 class Movie():
-  """Summary of class here.
-  Attributes:
-      title: Movie title.
-      storyline: Description of the movie story.
-      poster_image_url: Url of the movie poster.
-      trailer_youtube_url: Url of the movie youtube trailer.
-  """
+    """Summary of class here.
+        Attributes:
+        title: Movie title.
+        storyline: Description of the movie story.
+        poster_image_url: Url of the movie poster.
+        trailer_youtube_url: Url of the movie youtube trailer.
+    """
 
-  def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-    self.title = movie_title
-    self.storyline = movie_storyline
-    self.poster_image_url = poster_image
-    self.trailer_youtube_url = trailer_youtube
+    def __init__(self, movie_title, storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
 
-  def show_trailer(self):
-    webbrowser.open(self.trailer_youtube_url)
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
