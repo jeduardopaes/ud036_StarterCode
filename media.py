be901fa9bd8b8a4366fb35ed5cf990e261ee73c4
Jose Eduardo Paes <j.eduardopaes@gmail.com>
2018-11-17 18:08:24 -0200

    Adding comment to class Movie

--- media.py
+++ media.py
@@ -1,6 +1,13 @@
 import webbrowser
 
 class Movie():
+  """Summary of class here.
+  Attributes:
+      title: Movie title.
+      storyline: Description of the movie story.
+      poster_image_url: Url of the movie poster.
+      trailer_youtube_url: Url of the movie youtube trailer.
+  """
 
   def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
     self.title = movie_title
